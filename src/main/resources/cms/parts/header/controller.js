83e53ee7bc8b25d77e7e18a11306819cdceaa6b7
mla <mla@enonic.com>
2015-02-27 16:17:59 -0800

    Updated the header part to work for authors list.

--- src/main/resources/cms/parts/header/controller.js
+++ src/main/resources/cms/parts/header/controller.js
@@ -26,6 +26,20 @@ exports.get = function(req) {
         headerText = 'Search Results for: ' + up.s;
     }
 
+    if (up.author) {
+        headerText = 'Author Archives: ';
+        var authorContent;
+        if (stk.content.exists(up.author)) {
+            authorContent = stk.content.get(up.author);
+            var url = execute('portal.pageUrl', {
+                path: stk.content.getPath(site._path),
+                params: { author: up.author }
+            });
+
+            headerText += '<a href="' + url + '">' + authorContent.data.name + '</a>';
+        }
+    }
+
 
     var params = {
         site: site,
