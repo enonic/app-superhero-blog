9f47c2c2e42796aac74ac3d8093a35049756b921
mla <mla@enonic.com>
2015-03-14 02:35:30 -0700

    Small changes for Facebook parts.

--- src/main/resources/cms/pages/default/controller.js
+++ src/main/resources/cms/pages/default/controller.js
@@ -8,7 +8,11 @@ function handleGet(req) {
     var menuItems = menu.getSiteMenu(3);
     var moduleConfig = site.moduleConfigs[module.name];
     var content = execute('portal.getContent');
+
+    // For facebook module
     var facebookAppID = moduleConfig.facebookAppID.length > 1 ? moduleConfig.facebookAppID : null; // Hack to prevent empty string
+    var userLanguage = req.headers['Accept-Language'].substring(0, req.headers['Accept-Language'].indexOf(','));
+
     var googleUA = moduleConfig.googleUA.length > 1 ? moduleConfig.googleUA : null;
     var bodyClass = '';
     var backgroundImage;
@@ -45,6 +49,7 @@ function handleGet(req) {
         content: content,
         menuItems: menuItems,
         facebookAppID: facebookAppID,
+        userLanguage: userLanguage,
         googleUA: googleUA
     }
 
