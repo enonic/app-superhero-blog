1286205220ac699783f4cbd80b2e9769e904139a
Espen Norderud <espen42@gmail.com>
2020-11-13 17:32:00 +0100

    #45: refactor var in meta.js --> const, let, function

--- src/main/resources/site/parts/meta/meta.js
+++ src/main/resources/site/parts/meta/meta.js
@@ -1,31 +1,29 @@
-var stk = require('/lib/stk/stk');
-var portal = require('/lib/xp/portal');
-var auth = require('/lib/xp/auth');
-var contentLib = require('/lib/xp/content');
+const stk = require('/lib/stk/stk');
+const portal = require('/lib/xp/portal');
+const auth = require('/lib/xp/auth');
+const contentLib = require('/lib/xp/content');
 
-exports.get = handleGet;
-
-function handleGet(req) {
+exports.get = function handleGet(req) {
 
     function renderView() {
 
-        var view = resolve('meta.html');
-        var model = createModel();
+        const view = resolve('meta.html');
+        const model = createModel();
         return stk.view.render(view, model);
     }
 
     function createModel() {
-        var component = portal.getComponent();
-        var config = component.config;
-        var title = config.title || 'Meta';
-        var user = auth.getUser();
-        var site = portal.getSite();
-        var idProviderKey = portal.getIdProviderKey();
+        const component = portal.getComponent();
+        const config = component.config;
+        const title = config.title || 'Meta';
+        const user = auth.getUser();
+        const site = portal.getSite();
+        const idProviderKey = portal.getIdProviderKey();
 
         // items need url, text and linkTitle
-        var items = [];
+        const items = [];
 
-        var item = {};
+        const item = {};
 
         if(user) {
             item.text = 'Log out';
@@ -43,19 +41,18 @@ function handleGet(req) {
             items.push(item);
         }
 
-        var links = config.links;
-
+        let links = config.links;
         if(links) {
             links = stk.data.forceArray(links);
 
-            for (var i = 0; i < links.length; i++) {
-                var item = {};
+            for (let i = 0; i < links.length; i++) {
+                const item = {};
                 item.text = links[i].linkText;
 
                 if(links[i].contentLink) {
                     item.url = portal.pageUrl({id: links[i].contentLink});
                     if(!links[i].linkText) {
-                        var linkContent = contentLib.get({key: links[i].contentLink});
+                        const linkContent = contentLib.get({key: links[i].contentLink});
                         if(linkContent) {
                             item.text = linkContent.displayName;
                         }
