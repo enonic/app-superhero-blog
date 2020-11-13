285fb04a1b867524590a7300b2fe88663cd64c64
Espen Norderud <espen42@gmail.com>
2020-11-13 17:18:48 +0100

    #45: refactor var in two-column.js --> const, let, function

--- src/main/resources/site/layouts/two-column/two-column.js
+++ src/main/resources/site/layouts/two-column/two-column.js
@@ -1,19 +1,17 @@
-var stk = require('/lib/stk/stk');
-var portal = require('/lib/xp/portal');
+const stk = require('/lib/stk/stk');
+const portal = require('/lib/xp/portal');
 
-exports.get = handleGet;
+exports.get = function(req) {
 
-function handleGet(req) {
+    const component = portal.getComponent();
 
-    var component = portal.getComponent();
-
-    var params = {
+    const params = {
         component: component,
         leftRegion: component.regions["left"],
         rightRegion: component.regions["right"]
     };
 
-    var view = resolve('two-column.html');
+    const view = resolve('two-column.html');
     return stk.view.render(view, params);
 
 }
