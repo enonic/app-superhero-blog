2b814bc463bfc47b1b7e0bc12c7627b6c8bf8abe
Espen Norderud <espen42@gmail.com>
2020-11-13 13:51:44 +0100

    #45, cleanup: fix bugs caused by the refactoring, remove excess logs. Main page controller refactored.

--- src/main/resources/site/pages/default/default.js
+++ src/main/resources/site/pages/default/default.js
@@ -1,15 +1,17 @@
 var thymeleaf = require('/lib/thymeleaf');
 var menu = require('/lib/menu');
-var stk = require('/lib/stk');
+var stk = require('/lib/stk/stk');
 var portal = require('/lib/xp/portal');
-
 
 var view = resolve('default.html');
 
+
+
+// Creates an assetUrls object on the first rendering.
+// It provides the URLs to all assets used in this page controller and its templates/fragments:
 var assetUrls = null;
 function getAssetUrls() {
     if (!assetUrls) {
-        log.info("Building assetUrls....");
         var assetUrls = {
             html5Js: portal.assetUrl({path: 'js/html5.js'}),
             styleCss: portal.assetUrl({path: 'css/style.css'}),
@@ -18,43 +20,12 @@ function getAssetUrls() {
             fontCarroisCss: portal.assetUrl({path: 'css/google-font-carrois-gothic.css'}),
             jqueryJs: portal.assetUrl({path: 'js/jquery-3.3.1.min.js'}),
             superheroJs: portal.assetUrl({path: 'js/superhero.js'}),
-            flexsliderJs: portal.assetUrl({path: 'js/flexslider-min.js'}),
+            flexsliderJs: portal.assetUrl({path: 'js/jquery.flexslider-min.js'}),
             smallMenuJs: portal.assetUrl({path: 'js/small-menu.js'}),
             commentReplyJs: portal.assetUrl({path: 'js/comment-reply.min.js'}),
         };
-        log.info("Ok.")
-        log.info("assetUrls (" +
-            (Array.isArray(assetUrls) ?
-                    ("array[" + assetUrls.length + "]") :
-                    (typeof assetUrls + (assetUrls && typeof assetUrls === 'object' ? (" with keys: " + JSON.stringify(Object.keys(assetUrls))) : ""))
-            ) + "): " + JSON.stringify(assetUrls, null, 2)
-        );
     }
     return assetUrls;
-}
-
-
-function buildPageContributions(siteConfig) {
-    if (siteConfig.backgroundImage) {
-        var bgImageUrl = portal.imageUrl({
-            id: siteConfig.backgroundImage,
-            scale: '(1,1)',
-            format: 'jpeg'
-        });
-
-        return {
-            headEnd: '<style type="text/css" id="custom-background-css">' +
-                        'body.custom-background { ' +
-                    'background-image: url("' + bgImageUrl + '");' +
-                    'background-repeat: repeat; ' +
-                    'background-position: top left; ' +
-                    'background-attachment: scroll; ' +
-                    '}' +
-                    '</style>'
-        };
-    }
-
-    return undefined;
 }
 
 
@@ -84,8 +55,9 @@ function getPageItemClass(targetPath, currentContentPath) {
     return targetPath == currentContentPath ? 'current_page_item' : 'page_item';
 }
 
-function insertMenuItemPageUrls(menuitems, contentPath) {
-    for (menuItem of menuitems) {
+
+function insertPageurlsAndClassesIntoMenuItems(menuItems, contentPath) {
+    for (menuItem of menuItems) {
         menuItem.pageUrl = portal.pageUrl({path: menuItem.path});
         menuItem.class = getPageItemClass(menuItem.path, contentPath);
 
@@ -99,25 +71,15 @@ function insertMenuItemPageUrls(menuitems, contentPath) {
 }
 
 
-exports.get = function handleGet(req) {
-    var params = req.params;
+exports.get = function handleGet(request) {
     var site = portal.getSite();
     var siteConfig = portal.getSiteConfig();
     stk.data.deleteEmptyProperties(siteConfig);
     var content = portal.getContent();
-    log.info("content (" +
-    	(Array.isArray(content) ?
-    		("array[" + content.length + "]") :
-    		(typeof content + (content && typeof content === 'object' ? (" with keys: " + JSON.stringify(Object.keys(content))) : ""))
-    	) + "): " + JSON.stringify(content, null, 2)
-    );
 
 
     var menuItems = menu.getSiteMenu(3);
-    insertMenuItemPageUrls(menuItems, content._path);
-
-
-    var footerText = siteConfig.footerText ? portal.processHtml({value: siteConfig.footerText}) : null;
+    insertPageurlsAndClassesIntoMenuItems(menuItems, content._path);
 
 
     var isFragment = content.type === 'portal:fragment';
@@ -125,32 +87,26 @@ exports.get = function handleGet(req) {
         assetUrls: getAssetUrls(),
         title: site.displayName,
         description: site.data.description,
-        bodyClass: buildBodyClass(site, siteConfig, content, params),
+        bodyClass: buildBodyClass(site, siteConfig, content, request.params),
         mainRegion: isFragment ? null : content.page.regions['main'],
         isFragment: isFragment,
         siteUrl: portal.pageUrl({path: site._path}),
         menuTopClass: getPageItemClass(site._path, content._path),
         menuItems: menuItems,
-        footerText: footerText,
-        headerStyle: req.mode == 'edit' ? 'position: absolute;' : null
+        footerText: siteConfig.footerText ?
+            portal.processHtml({value: siteConfig.footerText}) :
+            null,
+        backgroundImageUrl: (siteConfig.backgroundImage) ?
+            portal.imageUrl({
+                id: siteConfig.backgroundImage,
+                scale: '(1,1)',
+                format: 'jpeg'
+            }) :
+            null,
+        headerStyle: request.mode == 'edit' ? 'position: absolute;' : null
     };
 
-    log.info("model (" +
-    	(Array.isArray(model) ?
-    		("array[" + model.length + "]") :
-    		(typeof model + (model && typeof model === 'object' ? (" with keys: " + JSON.stringify(Object.keys(model))) : ""))
-    	) + "): " + JSON.stringify(model, null, 2)
-    );
-
-    log.info("pageContributions (" +
-    	(Array.isArray(buildPageContributions(siteConfig)) ?
-    		("array[" + buildPageContributions(siteConfig).length + "]") :
-    		(typeof buildPageContributions(siteConfig) + (buildPageContributions(siteConfig) && typeof buildPageContributions(siteConfig) === 'object' ? (" with keys: " + JSON.stringify(Object.keys(buildPageContributions(siteConfig)))) : ""))
-    	) + "): " + JSON.stringify(buildPageContributions(siteConfig), null, 2)
-    );
-
     return {
-        body: thymeleaf.render(view, model),
-        pageContributions: buildPageContributions(siteConfig)
+        body: thymeleaf.render(view, model)
     }
 };
