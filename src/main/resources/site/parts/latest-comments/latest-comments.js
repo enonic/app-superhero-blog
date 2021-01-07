ae682894a263d19d1c77ad4df035938c48959e27
Espen Norderud <espen42@gmail.com>
2021-01-08 00:33:52 +0100

    #89: Refactored the fetch-comments-data section.

--- src/main/resources/site/parts/latest-comments/latest-comments.js
+++ src/main/resources/site/parts/latest-comments/latest-comments.js
@@ -11,25 +11,48 @@ const DEFAULTS = {
     headline: "Latest comments"
 };
 
-exports.get = function (req) {
-    const content = portal.getComponent();
 
-    const siteContent = portal.getContent();
+/**
+ *   Extract and format comment nodes where that comment has a parent content (i.e. a post)
+ *   that exists / is not deleted (in which case contentLib.get would return null)
+ *   into an array of comment objects:
+ */
+function extractComments(commentNodes) {
+    const comments = []
+    for (let j = 0; j< commentNodes.length; j++) {
+        const commentNode = commentNodes[j];
 
-    const size = content.config.size || DEFAULTS.size;
-    const headline = ((content.config.headline || '') + '').trim() || DEFAULTS.headline;
+        const parent = contentLib.get({
+            // Extra safeguard: if commentNode is missing/empty, parent will just become null and be filtered out below:
+            key: (commentNode || {}).content
+        });
 
-    let langCode = siteContent.language;
-    if (langCode)
-        langCode = langCode.replace(/_/g, '-'); //replace all underscore with dash
+        if (parent) {
+            // By now, we know the comment node is usable.
+            // Extract and format data from it, and push that comment object into the usableComments array:
+            const comment = commentLib.getNodeData(commentNode);
+            comment.contentUrl = portal.pageUrl({ id: commentNode.content });
+            comment.contentName = parent.displayName;
+            comments.push(comment);
+        }
+    }
 
-    const connection = nodeLib.connect({
+    return comments;
+}
+
+
+/**
+ * Use the node API to query the comments repo for raw data of the latest comments on the entire site.
+ */
+function queryCommentNodes(size) {
+    const commentRepo = nodeLib.connect({
         repoId: commentLib.REPO_ID,
         branch: "master",
+        principals: ["role:system.admin"],
     });
 
-    //query the latest comments here
-    const result = connection.query({
+    // query: IDs of the latest comments:
+    const result = commentRepo.query({
         start: 0,
         count: size,
         query: "",
@@ -54,27 +77,34 @@ exports.get = function (req) {
         },
     });
 
-    const comments = [];
-    let hasComments = false;
+    // Gather the found comment IDs in one array:
+    const commentIds = [];
     for (let i = 0; i < result.hits.length; i++) {
-        const commentId = result.hits[i].id;
-        const node = connection.get(commentId);
-
-        const nodeContext = contentLib.get({ key: node.content });
-        //If content is deleted we cant use it.
-        if (!nodeContext) {
-            comments[i] = null;
-            break;
-        }
-        comments[i] = commentLib.getNodeData(node);
-        comments[i].contentUrl = portal.pageUrl({ id: node.content });
-        comments[i].contentName = contentLib.get({ key: node.content }).displayName;
-
-        hasComments = true;
+        commentIds.push(result.hits[i].id);
     }
 
+    // query: fetch their comment data nodes with one query (ID-array argument is faster than many single queries),
+    return commentRepo.get(commentIds);
+}
+
+
+exports.get = function (req) {
+    const content = portal.getComponent();
+
+    const siteContent = portal.getContent();
+
+    const size = content.config.size || DEFAULTS.size;
+    const headline = ((content.config.headline || '') + '').trim() || DEFAULTS.headline;
+
+    let langCode = siteContent.language;
+    if (langCode)
+        langCode = langCode.replace(/_/g, '-'); //replace all underscore with dash
+
+    const commentNodes = queryCommentNodes(size);
+    const comments = extractComments(commentNodes);
+
     const model = {
-        hasComments: hasComments,
+        hasComments: comments.length > 0,
         comments: comments,
         headline: headline,
         local: {
