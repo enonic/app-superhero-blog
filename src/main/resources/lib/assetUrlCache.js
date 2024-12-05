08382c7e6a7e028e18b1874d58b385eb60546447
Sergey Rymsha <rymsha@users.noreply.github.com>
2024-12-05 09:35:32 +0100

    Migrate to lib-asset #143 (#144)

--- src/main/resources/lib/assetUrlCache.js
+++ src/main/resources/lib/assetUrlCache.js
@@ -1,7 +1,7 @@
 // Creates an assetUrls object on the first rendering unique to viewmode/branch.
 // It provides the URLs to all assets used in this page controller and its templates/fragments.
 // Will update when app is restarted/refreshed.
-const portal = require('/lib/xp/portal');
+const assetLib = require('/lib/enonic/asset');
 
 const assetUrls = {};
 
@@ -9,17 +9,17 @@ exports.getAssetUrls = function(viewmode, branch) {
     const key = `${branch}_${viewmode}`;
     if (!assetUrls[key]) {
         assetUrls[key] = {
-            html5Js: portal.assetUrl({path: 'js/html5.js'}),
-            styleCss: portal.assetUrl({path: 'css/style.css'}),
-            enonicCss: portal.assetUrl({path: 'css/enonic.css'}),
-            flexsliderCss: portal.assetUrl({path: 'css/flexslider.css'}),
-            fontCarroisCss: portal.assetUrl({path: 'css/google-font-carrois-gothic.css'}),
-            buttonsCss: portal.assetUrl({path: 'css/buttons.min.css'}),
+            html5Js: assetLib.assetUrl({path: 'js/html5.js'}),
+            styleCss: assetLib.assetUrl({path: 'css/style.css'}),
+            enonicCss: assetLib.assetUrl({path: 'css/enonic.css'}),
+            flexsliderCss: assetLib.assetUrl({path: 'css/flexslider.css'}),
+            fontCarroisCss: assetLib.assetUrl({path: 'css/google-font-carrois-gothic.css'}),
+            buttonsCss: assetLib.assetUrl({path: 'css/buttons.min.css'}),
 
-            jqueryJs: portal.assetUrl({path: "js/lib/jquery-3.3.1.min.js"}),
-            superheroJs: portal.assetUrl({path: 'js/superhero.js'}),
-            flexsliderJs: portal.assetUrl({path: 'js/lib/jquery.flexslider-min.js'}),
-            commentsJs: portal.assetUrl({ path: "js/comments.js"})
+            jqueryJs: assetLib.assetUrl({path: "js/lib/jquery-3.3.1.min.js"}),
+            superheroJs: assetLib.assetUrl({path: 'js/superhero.js'}),
+            flexsliderJs: assetLib.assetUrl({path: 'js/lib/jquery.flexslider-min.js'}),
+            commentsJs: assetLib.assetUrl({ path: "js/comments.js"})
         };
     }
     return assetUrls[key];
